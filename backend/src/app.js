0d4b5c4cbf5b6b10e86c4c5e6a1dd9c785342a79
Guilherme Bueno de Campos <guibueno23102001@gmail.com>
2026-05-19 18:58:35 -0300

    fix: adiciona middleware CORS adicional para suportar x-guest-token

--- backend/src/app.js
+++ backend/src/app.js
@@ -70,6 +70,23 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// Middleware adicional para garantir headers CORS
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.has(normalizeOrigin(origin))) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Guest-Token,x-guest-token,Accept,Accept-Language');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  }
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
