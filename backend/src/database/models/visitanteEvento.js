99f7f10749b8fe10aa0864a92b6a0187db376b95
Guilherme Bueno de Campos <guibueno23102001@gmail.com>
2026-05-25 14:55:06 -0300

    fix: map criado_em field to created_at column in database

--- backend/src/database/models/visitanteEvento.js
+++ backend/src/database/models/visitanteEvento.js
@@ -38,6 +38,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.DATE,
         allowNull: false,
         defaultValue: DataTypes.NOW,
+        field: 'created_at',
       },
     },
     {
